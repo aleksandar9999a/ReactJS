279922e98e6c793e3478e518cea7ffc088e8a66a
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-03 11:06:17 +0200

    Increment Counter Solution

--- Intro to React.js/react-03/src/App.js
+++ Intro to React.js/react-03/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let counter = 0;
+
+const events = {
+  handleEvent: function (e) {
+    if (e.target.tagName === 'BUTTON') {
+      counter++;
+      e.target.innerHTML = counter;
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>{counter}</button>
       </header>
     </div>
   );
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('click', events)
+})
 
 export default App;
