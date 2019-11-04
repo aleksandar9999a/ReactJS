14e570ceda7f0585c9a2eb43cc3ed96f988d9f0f
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-04 10:38:46 +0200

    Added second form for password

--- Events and Forms/demo/src/components/RegisterForm.js
+++ Events and Forms/demo/src/components/RegisterForm.js
@@ -4,7 +4,10 @@ export default class RegisterForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: ''
+            user: {
+                username: '',
+                password: ''
+            }
         }
 
         this.onInputChange = this.onInputChange.bind(this);
@@ -12,21 +15,28 @@ export default class RegisterForm extends Component {
     }
 
     onInputChange(e) {
-        this.setState(
-            { value: e.target.value }
-        )
+        let user = this.state.user;
+        let inputName = e.target.name;
+        let inputValue = e.target.value;
+
+        user[inputName] = inputValue;
+
+        this.setState({user});
     }
 
     onFormSubmit(e) {
         e.preventDefault();
-        console.log(this.state.value);
+        console.log(this.state);
     }
 
     render() {
         return (
             <form onSubmit={this.onFormSubmit}>
                 UserName:
-                <input type='text' name='username' onChange={this.onInputChange} />
+                <input type='text' name='username' value={this.state.user.username} onChange={this.onInputChange} />
+                <br />
+                Password:
+                <input type='password' name='password' value={this.state.user.password} onChange={this.onInputChange} />
                 <br />
                 <input type='submit' />
             </form>
