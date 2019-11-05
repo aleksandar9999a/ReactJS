bde06f72f335b01222083210fc24aca9314a3381
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-05 20:00:42 +0200

    Added state to PokemonForm

--- Events and Forms-Exercises/pokemon-roster/src/components/PokemonForm/PokemonForm.js
+++ Events and Forms-Exercises/pokemon-roster/src/components/PokemonForm/PokemonForm.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 
-export default class PokemonForm extends Component{
-    render(){
+export default class PokemonForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    getImages() {
+
+    }
+
+    render() {
         return (
             <div>
-                <h1>Logged</h1>
-                Pokemon Name:
+                <div>
+                    <h1>Logged</h1>
+                    Pokemon Name:
                 <br />
-                <input type='text' name='pokemonName' />
+                    <input type='text' name='pokemonName' />
+                    <br />
+                    Pokemon image:
                 <br />
-                Pokemon image:
+                    <input type='link' name='pokemonImg' />
+                    <br />
+                    Pokemon Info:
                 <br />
-                <input type='link' name='pokemonImg' />
-                <br />
-                Pokemon Info:
-                <br />
-                <input type='text' name='pokemonInfo' />
-                <br />
-                <button type='submit' >Create Pokemon</button>
+                    <input type='text' name='pokemonInfo' />
+                    <br />
+                    <button type='submit' >Create Pokemon</button>
+                </div>
+                <div>
+
+                </div>
             </div>
         )
     }
