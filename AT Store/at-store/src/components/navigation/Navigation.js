bc89f7881cb5e80cdaa85b7022fb83269a14535a
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-07 21:48:51 +0200

    Added navbar style

--- AT Store/at-store/src/components/navigation/Navigation.js
+++ AT Store/at-store/src/components/navigation/Navigation.js
@@ -7,12 +7,12 @@ export default class Navigation extends Component {
         <Navbar bg='dark' variant='dark'>
             <Navbar.Brand href="/">AT-Store</Navbar.Brand>
             <Nav className='mx-auto'>
-                <NavLink to="/home">Home</NavLink>
-                <NavLink to="/products">Products</NavLink>
-                <NavLink to="/about">About</NavLink>
+                <NavLink to="/home" className="nav-link">Home</NavLink>
+                <NavLink to="/products" className="nav-link">Products</NavLink>
+                <NavLink to="/about" className="nav-link">About</NavLink>
             </Nav>
             <Nav className=''>
-            <NavLink to="/login">Log In/Registetion</NavLink>
+            <NavLink to="/login" className="btn btn-outline-success my-2 my-sm-0">Log In/Registetion</NavLink>
             </Nav>
         </Navbar>
     )
