ed6d8153cc8796e9ab2329d0535c7eaf1d0fbd8e
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-07 20:42:23 +0200

    Function who generate img tags

--- AT Store/at-store/src/components/carousel/Carousel.js
+++ AT Store/at-store/src/components/carousel/Carousel.js
@@ -5,13 +5,26 @@ import CarouselButton from './CarouselButton';
 
 
 export default class Carousel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { images: [] }
+    }
+
+    fetchImages = () => {
+
+    }
+
+    componentDidMount = () => {
+        this.fetchImages();
+    }
+
     render = () => (
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <CarouselIndicators />
             </ol>
             <div class="carousel-inner">
-                <CarouselImage />
+                <CarouselImage images={this.state.images} />
             </div>
             <CarouselButton data-slide="prev" spanClass="carousel-control-prev-icon" text='Previous' />
             <CarouselButton data-slide="next" spanClass="carousel-control-next-icon" text='Next' />
--- AT Store/at-store/src/components/carousel/CarouselImage.js
+++ AT Store/at-store/src/components/carousel/CarouselImage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 export default class CarouselImage extends Component {
-    render = () => (
-        <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="..." />
-        </div>
-    )
+    generateImgTag = ({ url }) => <img src={url} class="d-block w-100" alt="..." />
+
+    render = () => {
+        const images = this.props.images(generateImgTag);
+        return (
+            <div class="carousel-item active">
+                {images}
+            </div>
+        )
+    }
 }
