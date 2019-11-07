614b1a0d053dea7ed866db12c2f3a6947b2b7574
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-07 20:20:28 +0200

    Update Basic Structure

--- AT Store/at-store/src/components/navigation/Navigation.js
+++ AT Store/at-store/src/components/navigation/Navigation.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export default class Navigation extends Component {
     render = () => (
-        <nav className="navbar navbar-dark bg-dark">
-            <nav className="navbar navbar-dark bg-dark">
-                <Link to='/' className="navbar-brand">AT-Store</Link>
-            </nav>
-        </nav>
+        <Navbar bg='dark' variant='dark'>
+            <Navbar.Brand href="/" style={{'width': '0px'}}>AT-Store</Navbar.Brand>
+            <Nav className='mx-auto'> 
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/">Products</Nav.Link>
+                <Nav.Link href="/">About</Nav.Link>
+            </Nav>
+        </Navbar>
     )
 }
