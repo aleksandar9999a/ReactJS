c5a4120aee6f102e299ea711a5c244949201b40b
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-08 11:27:41 +0200

    Added handleChages for Log In Form who handle changes in inputs and checkbox

--- AT Store/at-store/src/components/loginForm/Login.js
+++ AT Store/at-store/src/components/loginForm/Login.js
@@ -6,25 +6,38 @@ export default class Login extends Component {
         super(props);
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            checkbox: false
         }
     }
+
+    handleChanges = (e) => {
+        let type = e.target.type;
+        let value = e.target.value
+
+        if (type === 'checkbox') {
+            value = e.target.checked;
+        }
+
+        this.setState({[type]: value});
+    }
+
     render = () => (
         <div className='mx-5'>
             <h1>Log In</h1>
             <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Enter email" onChange={this.handleChanges} />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={this.handleChanges} />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+                    <Form.Check type="checkbox" label="Check me out" onChange={this.handleChanges} />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Log In
