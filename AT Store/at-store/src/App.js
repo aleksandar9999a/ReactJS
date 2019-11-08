b5c241ef4cfe1530707cc25a557f3b2e25b7440d
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-08 14:58:56 +0200

    Remove big part from check for Loged User (don't work properly)

--- AT Store/at-store/src/App.js
+++ AT Store/at-store/src/App.js
@@ -4,20 +4,10 @@ import Footer from './components/footer/Footer';
 import MyRouters from './components/myRouters/MyRouters';
 
 export default class App extends Component {
-  handleLoggedUser = () => {
-    let authtoken = sessionStorage.getItem('authtoken');
-    
-    if (authtoken) {
-      return true;
-    }
-
-    return false;
-  }
-
   render = () => {
     return (
       <div className="App">
-        <Navigation isLogged={this.handleLoggedUser}/>
+        <Navigation />
         <MyRouters />
         <Footer />
       </div>
--- AT Store/at-store/src/components/navigation/Navigation.js
+++ AT Store/at-store/src/components/navigation/Navigation.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import requester from './../../tools/requester';
 
 export default class Navigation extends Component {
-    renderLoginButton = () => {
-        if (this.props.isLogged) {
-            return (
-                <Nav>
-                    <Link to="/login" className="btn btn-outline-success my-2 my-sm-0">Log In/Sing In</Link>
-                </Nav>
-            )
-        }
-
-        return null
+    logOut = () => {
+        requester
+            .post('user', '_logout', 'kinvey')
+            .then(res => {
+                sessionStorage.removeItem('authtoken')
+            })
     }
+
     render = () => (
         <Navbar bg='dark' variant='dark'>
             <Link to="/" className="navbar-brand">AT-Store</Link>
@@ -22,7 +20,9 @@ export default class Navigation extends Component {
                 <Link to="/products" className="nav-link">Products</Link>
                 <Link to="/about" className="nav-link">About</Link>
             </Nav>
-            {this.renderLoginButton()}
+            <Nav>
+                <Link to="/login" className="btn btn-outline-success my-2 my-sm-0">Log In/Sing In</Link>
+            </Nav>
         </Navbar>
     )
 }
