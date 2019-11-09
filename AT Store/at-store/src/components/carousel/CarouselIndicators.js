09f7db1ee4e705a84a170fae6578084b708c7db4
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-09 19:01:42 +0200

    Remove AT Store From ReactJS

--- AT Store/at-store/src/components/carousel/CarouselIndicators.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class CarouselIndicators extends Component{
-    render = () => (
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-    )
-}
